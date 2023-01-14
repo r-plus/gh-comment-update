25d98a45269c0ccff234012fe21e526d037c26bb
r-plus <anasthasia.r@gmail.com>
2023-01-14 19:08:21 +0900

    first implement

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/r-plus/gh-comment-update
 
-go 1.18
+go 1.19
 
 require github.com/cli/go-gh v1.0.0
 
--- main.go
+++ main.go
@@ -1,26 +1,126 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"regexp"
 
 	"github.com/cli/go-gh"
+	"github.com/cli/go-gh/pkg/repository"
 )
 
 func main() {
-	fmt.Println("hi world, this is the gh-comment-update extension!")
-	client, err := gh.RESTClient(nil)
-	if err != nil {
-		fmt.Println(err)
-		return
+	if err := cli(); err != nil {
+		fmt.Fprintf(os.Stderr, "gh-comment-update failed: %s\n", err.Error())
+		os.Exit(1)
 	}
-	response := struct {Login string}{}
-	err = client.Get("user", &response)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("running as %s\n", response.Login)
 }
 
-// For more examples of using go-gh, see:
-// https://github.com/cli/go-gh/blob/trunk/example_gh_test.go
+func cli() error {
+	regexpFlag := flag.String("regexp", "", "Search first matched comment by regexp")
+	bodyFlag := flag.String("body", "", "Update body text")
+	repoOverride := flag.String(
+		"repo", "", "Specify a repository. If omitted, uses current repository")
+	flag.Parse()
+
+	if *regexpFlag == "" || *bodyFlag == "" {
+		return fmt.Errorf("regexp and body flags are required")
+	}
+
+	var repo repository.Repository
+	var err error
+
+	if *repoOverride == "" {
+		repo, err = gh.CurrentRepository()
+	} else {
+		repo, err = repository.Parse(*repoOverride)
+	}
+	if err != nil {
+		return fmt.Errorf("could not determine what repo to use: %w", err.Error())
+	}
+
+	client, err := gh.GQLClient(nil)
+	if err != nil {
+		return fmt.Errorf("could not create a graphql client: %w", err)
+	}
+	query := fmt.Sprintf(`{
+		repository(owner: "%s", name: "%s") {
+			id
+			issue(number: 1) {
+				id
+				comments(first: 100) {
+					edges {
+						node {
+							id
+							body
+							viewerDidAuthor
+						}
+					}
+				}
+			}
+		}
+	}`, repo.Owner(), repo.Name())
+
+	type Comment struct {
+		Id              string
+		Body            string
+		ViewerDidAuthor bool
+	}
+
+	response := struct {
+		Repository struct {
+			Issue struct {
+				Comments struct {
+					Edges []struct {
+						Node Comment
+					}
+				}
+			}
+		}
+	}{}
+
+	err = client.Do(query, nil, &response)
+	if err != nil {
+		return fmt.Errorf("failed to talk to the GitHub API: %w", err)
+	}
+	var match *Comment
+
+	r := regexp.MustCompile(*regexpFlag)
+
+	for _, edge := range response.Repository.Issue.Comments.Edges {
+		if edge.Node.ViewerDidAuthor && r.MatchString(edge.Node.Body) {
+			match = &edge.Node
+			break
+		}
+	}
+	if match == nil {
+		fmt.Fprintln(os.Stderr, "No matching comment found.")
+		return nil
+	}
+
+	// mutation
+
+	var m struct {
+		UpdateIssueComment struct {
+			ClientMutationId string
+		} `graphql:"updateIssueComment(input: $input)"`
+	}
+	type UpdateIssueCommentInput struct {
+		ID   string `json:"id"`
+		Body string `json:"body"`
+	}
+	variables := map[string]interface{}{
+		"input": UpdateIssueCommentInput{
+			ID:   string(match.Id),
+			Body: string(*bodyFlag),
+		},
+	}
+
+	err = client.Mutate("gh_comment_update", &m, variables)
+	if err != nil {
+		return fmt.Errorf("failed to mutate comment: %w", err)
+	}
+
+	return nil
+}
